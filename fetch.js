dedf13eca1680c5bbc230823487fcb2ee67eb4b3
Punit <punitmahes@gmail.com>
2021-06-22 16:51:45 +0530

    Testing web for extension

---
+++ fetch.js
@@ -0,0 +1,25 @@
+/** 
+ * Helper function to call web API endpoint
+ * using the authorization bearer token scheme
+*/
+function callApiWithToken(endpoint, token, callback) {
+    const headers = new Headers();
+    const bearer = `Bearer ${token}`;
+
+    headers.append("Authorization", bearer);
+    const res  = { query: "Select [System.Id], [System.Title], [System.State] From WorkItems Where [System.WorkItemType] = 'Bug' order by [Microsoft.VSTS.Common.Priority] asc, [System.CreatedDate] desc"};
+    const options = {
+        method: "GET",
+        headers: headers,
+    };
+    logMessage('Calling web API...');
+    fetch(endpoint, options)
+        .then(response => response.json())
+        .then(response => {
+            logMessage('Web API responds:');
+            logMessage(response);
+            console.log(response);
+        }).catch(error => {
+            console.error(error);
+        });
+}
